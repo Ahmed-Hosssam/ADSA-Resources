5ff5b8ea783eee12a7edaa2b6a89b62da42c58ed
ahmedhossam <ahmed.hosssam.elden@gmail.com>
2023-03-04 23:51:14 +0200

    Modified Scanner static class in Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
